3a6d8a0a6f09736c014d506fae150295c4c93ce2
CarterBunn-UW <cabunn@wisc.edu>
2025-11-20 13:21:54 -0600

    Imporved login visibility

--- src/components/nav/pages/RoastCoach.jsx
+++ src/components/nav/pages/RoastCoach.jsx
@@ -6,17 +6,25 @@ import StravaLoginStatusContext from '../../../contexts/StravaLoginStatusContext
 
 export default function RoastCoach() {
     const [authData, setAuthData] = useState(null);
+    const [loginVisible, setLoginVisible] = useState(true);
 
     return (
         <div>
             <div>
-                <StravaLoginStatusContext.Provider value={{ authData, setAuthData }}>
+                <StravaLoginStatusContext.Provider value={{ authData, setAuthData, setLoginVisible }}>
                     <RoastCoachNavbar />
-                    {authData ? (
-                        <Outlet />
-                    ) : (
-                        <StravaAuth setAuthData={setAuthData} />
-                    )}
+                    <div style={{
+                        width: "90%",
+                        margin: "0 auto",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        }}>
+                    {loginVisible ? (
+                        <StravaAuth setLoginVisible={setLoginVisible} />
+                    ) : <></>}
+                    <Outlet />
+                    </div>
                 </StravaLoginStatusContext.Provider>
             </div>
         </div>
