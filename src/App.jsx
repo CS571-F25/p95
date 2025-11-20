320eafcbed1097022801e4d964443a7e59368995
CarterBunn-UW <cabunn@wisc.edu>
2025-11-20 13:18:28 -0600

    Added profile and week in roasts page to app for routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { HashRouter, Routes, Route } from 'react-router'
 import AboutMe from './components/nav/pages/AboutMe'
 import Home from './components/nav/pages/Home'
 import RoastCoach from './components/nav/pages/RoastCoach'
+import Profile from './components/nav/pages/Profile'
+import WeekInRoasts from './components/nav/pages/WeekInRoasts'
 
 export default function App() {
   return <HashRouter>
@@ -10,6 +12,8 @@ export default function App() {
       <Route path='/' element={<RoastCoach/>}>
           <Route index element={<Home/>}></Route>
           <Route path='/about' element={<AboutMe/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+          <Route path='/week-review' element={<WeekInRoasts/>}></Route>
       </Route>
     </Routes>
   </HashRouter>
